47942794e54e3fbefa69f6ba254ede0eee0a0e8d
edus44 <eduhhg@gmail.com>
2018-12-28 09:13:34 +0100

    cypress 3.1.4

--- images/node-8-cypress/build.sh
+++ images/node-8-cypress/build.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
 
 NAME=gigigo/node-8-cypress
-TAG=3.1.3
+TAG=3.1.4
 
 echo "Building $NAME"
-docker build -t ${NAME}:${TAG} .
+# docker build -t ${NAME}:${TAG} --build-arg CYPRESS_VERSION=${TAG} .
 
-# docker push ${NAME}:${TAG}
+docker push ${NAME}:${TAG}
